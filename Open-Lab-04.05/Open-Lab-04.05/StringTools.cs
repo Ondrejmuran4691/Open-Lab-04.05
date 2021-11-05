97d791159782852c2b1182340ee9e26590b7e344
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-05 09:18:45 +0100

    multiples letters in string by n

---
+++ Open-Lab-04.05/Open-Lab-04.05/StringTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Open_Lab_04._05
+{
+    public class StringTools
+    {
+        public string Repeat(string orig, int n)
+        {
+            string done = "";
+            int i = 1;
+            foreach (char c in orig)
+            {
+                while(i <= n){ 
+                done += c.ToString();
+                    i++;
+                     }
+                i = 1;
+            } 
+            return done;
+        }
+    }
+}
